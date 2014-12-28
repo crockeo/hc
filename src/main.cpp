d7386f2ce15db8bed236b292f5bfbabdb646a7c0
Cerek Hillen <crockeo@gmail.com>
2014-12-28 14:28:08 -0500

    Made the thing use the assets object.

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,27 @@
 //////////
 // Code //
 
+// The assets!
+void initAssets(Assets& a) {
+    a.addAssetLoad("res/test.png", HC_SPRITE_ASSET);
+}
+
 // Entry point!
 int main() {
+    // Opening the window.
     Window w("Hello World!", 640, 480, false);
 
-    Sprite s(w.getRenderer(), "res/test.png");
+    // Loading and initializing assets.
+    Assets a;
+    initAssets(a);
+    a.performLoads(w);
 
     SDL_SetRenderDrawColor(w.getRenderer(), 255, 0, 255, 255);
     SDL_RenderClear(w.getRenderer());
 
     Rectangle r(640 / 2 - 450 / 2, 480 / 2 - 150 / 2, 450, 150);
     SDL_RenderCopy(w.getRenderer(),
-                   s.getTexture(),
+                   a.getSprite("res/test.png").getTexture(),
                    nullptr,
                    &r.sdlRect);
 
