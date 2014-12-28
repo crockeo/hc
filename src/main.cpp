8744fa70be77d982945735796756ea3a41777944
Cerek Hillen <crockeo@gmail.com>
2014-12-28 14:17:39 -0500

    :)

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,7 @@ int main() {
     SDL_SetRenderDrawColor(w.getRenderer(), 255, 0, 255, 255);
     SDL_RenderClear(w.getRenderer());
 
-    Rectangle r(0, 0, 300, 200);
-
+    Rectangle r(640 / 2 - 450 / 2, 480 / 2 - 150 / 2, 450, 150);
     SDL_RenderCopy(w.getRenderer(),
                    s.getTexture(),
                    nullptr,
