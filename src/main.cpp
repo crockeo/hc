59170693fffd500b40505ab101964902c8a8f16e
Cerek Hillen <crockeo@gmail.com>
2014-12-27 20:18:23 -0500

    Got a window creating.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 //////////////
 // Includes //
+#include <SDL.h>
 #include <iostream>
 
 //////////
@@ -7,6 +8,28 @@
 
 // Entry point!
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cout << "Failed to initialize SDL." << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Horizontally Challenged",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == nullptr) {
+        std::cout << "Failed to open an SDL_Window." << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
