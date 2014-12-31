e5342789bbfca4779b7dec4d0e6e2d2b122a4fec
Cerek Hillen <crockeo@gmail.com>
2014-12-31 11:09:55 -0500

    Mostly implemented a SpriteSheet.

---
+++ src/spritesheet.cpp
@@ -0,0 +1,60 @@
+#include "spritesheet.hpp"
+
+//////////
+// Code //
+
+// Initializing the SpriteSheet sizes.
+void SpriteSheet::init(int cols, int rows, int width, int height) {
+    this->cols   = cols;
+    this->rows   = rows;
+    this->width  = width;
+    this->height = height;
+}
+
+// Making a SpriteSheet from an existing SDL_Texture.
+SpriteSheet::SpriteSheet(SDL_Texture* tex,
+                         int cols , int rows,
+                         int width, int height) throw(HCException) {
+    this->sprite = new Sprite(tex);
+    init(cols, rows, width, height);
+}
+
+// Making a SpriteSheet from an SDL_Surface.
+SpriteSheet::SpriteSheet(SDL_Renderer* renderer, SDL_Surface* surf,
+                         int cols , int rows,
+                         int width, int height) throw(HCException) {
+    this->sprite = new Sprite(renderer, surf);
+    init(cols, rows, width, height);
+}
+
+// Loading a SpriteSheet from a location on the disk.
+SpriteSheet::SpriteSheet(SDL_Renderer* renderer, std::string path,
+                         int cols , int rows,
+                         int width, int height) throw(HCException) {
+    this->sprite = new Sprite(renderer, path);
+    init(cols, rows, width, height);
+}
+
+// The copy constructor.
+SpriteSheet::SpriteSheet(const SpriteSheet& ss) {
+    this->sprite = ss.sprite;
+    init(ss.rows, ss.cols, ss.width, ss.height);
+    this->original = false;
+}
+
+// Destroying the SpriteSheet.
+SpriteSheet::~SpriteSheet() {
+    if (this->original)
+        delete this->sprite;
+}
+
+
+// Blitting a specific tile.
+void SpriteSheet::blit(Window& w, Rectangle dst, int x, int y) {
+    // TODO: This.
+}
+
+// Blitting a portion of a specific tile.
+void SpriteSheet::blit(Window& w, Rectangle dst, Rectangle src, int x, int y) {
+    // TODO: This.
+}
---
+++ src/spritesheet.hpp
@@ -0,0 +1,46 @@
+#ifndef _SPRITE_SHEET_HPP_
+#define _SPRITE_SHEET_HPP_
+
+//////////////
+// Includes //
+#include "hcexception.hpp"
+#include "sprite.hpp"
+
+//////////
+// Code //
+
+// A type to specifically represent multiple sprites on a single texture.
+class SpriteSheet {
+private:
+    Sprite* sprite;
+    int cols , rows;
+    int width, height;
+    bool original;
+
+    // Initializing the SpriteSheet sizes.
+    void init(int, int, int, int);
+
+public:
+    // Making a SpriteSheet from an existing SDL_Texture.
+    SpriteSheet(SDL_Texture*, int, int, int, int) throw(HCException);
+
+    // Making a SpriteSheet from an SDL_Surface.
+    SpriteSheet(SDL_Renderer*, SDL_Surface*, int, int, int, int) throw(HCException);
+
+    // Loading a SpriteSheet from a location on the disk.
+    SpriteSheet(SDL_Renderer*, std::string, int, int, int, int) throw(HCException);
+
+    // The copy constructor.
+    SpriteSheet(const SpriteSheet&);
+
+    // Destroying the SpriteSheet.
+    ~SpriteSheet();
+
+    // Blitting a specific tile.
+    void blit(Window&, Rectangle, int, int);
+    
+    // Blitting a portion of a specific tile.
+    void blit(Window&, Rectangle, Rectangle, int, int);
+};
+
+#endif
