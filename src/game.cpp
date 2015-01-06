ab2793ce3b053a4d8c188196d6d4fdf7dd570a96
Cerek Hillen <crockeo@gmail.com>
2015-01-06 00:08:28 -0500

    Removed a lot of the extra rendering stuff.

--- src/game.cpp
+++ src/game.cpp
@@ -28,26 +28,10 @@ void game::update(GameState& g, float dt) {
 
 // Rendering the game.
 void game::render(GameState g, Window& w, const Assets& a) {
-    Rectangle r(0 - g.cam.x, 0 - g.cam.y, 64, 64);
-    Rectangle s(0, 0, 16, 16);
-    Rectangle r2(80 - g.cam.x, 0 - g.cam.y, 64, 64);
-    Rectangle r3(0 - g.cam.x, 80 - g.cam.y, 64, 64);
-    Rectangle r4(80 - g.cam.x, 80 - g.cam.y, 192, 64);
-
-    g.position.translate(-g.cam.x, -g.cam.y);
-
     SDL_RenderClear(w.getRenderer());
 
     g.player->render(w, g.cam, a);
     for (auto it = g.blocks.begin(); it != g.blocks.end(); it++)
         (*it)->render(w, g.cam, a);
-
-    a.sprites.at("res/forest_tiles.png").blit(w, r, s);
-    a.spriteSheets.at("res/forest_tiles.png").blit(w, r2, 0, 0);
-    a.animations.at("res/forest_tiles.png").blit(w, r3);
-    a.sprites.at("res/test.png").blit(w, r4);
-
-    a.animations.at("res/test.png").blit(w, g.position);
-
     SDL_RenderPresent(w.getRenderer());
 }
